f7f918a1f16776310d6ff7a3344dd650f61afaf8
Nguyen Sao Mai <nguyensmai@gmail.com>
2014-01-21 01:43:57 +0000

    hand-coded new world, the mapping between inputs and outputs are coded by hand (no randomization)

--- world.py
+++ world.py
@@ -32,11 +32,11 @@ class World():
         self.s = [0]*NUM_DIMENSIONS    #CURRENT STATE 
         self.stp1 = [0]*NUM_DIMENSIONS #TEMPORARY STATE. 
         #Create function sructures
-        self.f1 = buildNetwork(NUMI1, 10, NUMO1, hiddenclass=TanhLayer, bias=True)
-        self.f2 = buildNetwork(NUMI2, 10, NUMO2, hiddenclass=TanhLayer, bias=True)
-        self.indI = np.random.randint(0,NUM_DIMENSIONS+NUM_MOTORS, size=3*NUMI1+4*NUMI2)
-        self.indO = range(3*NUMO1+4*NUMO2);
-        shuffle(self.indO)
+        self.f1 = buildNetwork(NUMI1, 2, NUMO1, hiddenclass=TanhLayer, bias=True)
+        self.f2 = buildNetwork(NUMI2, 50, NUMO2, hiddenclass=TanhLayer, bias=True)
+        #self.indI = np.random.randint(0,NUM_DIMENSIONS+NUM_MOTORS, size=3*NUMI1+4*NUMI2)
+        #self.indO = range(3*NUMO1+4*NUMO2);
+        #shuffle(self.indO)
 
 
 
@@ -53,25 +53,25 @@ class World():
         dimI2 = self.f2['in'].dim;
         dimO1 = self.f1['out'].dim;
         dimO2 = self.f2['out'].dim;
-        sm    = self.s+m;
         output=[];
-        input = [];
         self.stp1=[]
 
-        for i in self.indI:
-            input.append(sm[i])
-
+        input    = m+self.s[0:NUMI1-2]+m+self.s[NUMI1-2:2*NUMI1-4]+m+self.s[2*NUMI1-4:3*NUMI1-6];
+        #for i in self.indI:
+        #    input.append(sm[i])
         for i in range(3):
             output[initOutput:initOutput+dimO1] = self.f1.activate(input[initInput:initInput+dimI1])
             initInput += dimI1;
             initOutput += dimO1;
 
+        initInput=0;
+        input    = m+self.s[-NUMI2+2:]+m+self.s[-2*NUMI2+4:-NUMI2+2]+m+self.s[-3*NUMI2+6:-2*NUMI2+4]+m+self.s[-4*NUMI2+8:-3*NUMI2+6];
         for i in range(4):
             output[initOutput:initOutput+dimO2] = self.f2.activate(input[initInput:initInput+dimI2])
             initInput += dimI2
             initOutput += dimO2;
 
-        for i in self.indO:
+        for i in range(NUM_DIMENSIONS):
             self.stp1.append(output[i])
 
         return self.stp1
