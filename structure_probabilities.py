905ad8947c4ee96b3235bb1ff7fdc5ea1c3028a2
Julie Kourtseva <julie.kourtseva@googlemail.com>
2014-01-31 19:35:42 +0000

    Finished writing tests for structure_probabilities

--- structure_probabilities.py
+++ structure_probabilities.py
@@ -1,21 +1,28 @@
+import math
+
 class StructureProbabilities(object):
 	def __init__(self, num_problems, num_hidden_layers, default_means=[5, 0], default_sds=[4, 1]):
-		if len(default_means) < len(num_hidden_layers):
-			default_means = default_means + [0]*(num_hidden_layers-len(default_means))
-			default_sds = default_sds + [1]*(num_hidden_layers-len(default_sds))
-		self.distribution = []
+		if len(default_means) < num_hidden_layers:
+			raise Exception("Not enough default values for mean and standard deviation")
+		self.distributions = []
 		means_sds = [list(x) for x in zip(default_means, default_sds)]
-		for i in num_problems:
+		for i in xrange(num_problems):
 			hl_probs = []
-			for j in num_hidden_layers:
+			for j in xrange(num_hidden_layers):
 				hl_probs.append(means_sds[j])
-			self.distribution.append(hl_probs)
+			self.distributions.append(hl_probs)
 
-	def update_probability(self, structure, problem, change_towards):
-		# For moving away from a number of hidden units (n),
-		# change mean by sd/(abs(mean-n)+1)*(sign(mean-n) depending on change_towards),
-		# increase sd.
-		# For moving towards it, change mean by sd/(abs(mean-n)+1)*sign(mean-n)
-		# and decrease sd
+	def get_mu_sigma(self, problem, hidden_layer):
+		return [None, None]
+
+	def set_mu_sigma(self, problem, hidden_layer):
+		return [None, None]
+
+	def update_probabilities(self, structure, problem, change_towards):
 		pass
 
+	def update_probability(self, value, mu, sigma, change_towards):
+		return [None, None]
+
+	def get_sample(self, problem, layer):
+		return None
--- test_structure_probabilities.py
+++ test_structure_probabilities.py
@@ -16,17 +16,79 @@ class TestStructureProbabilities(unittest.TestCase):
 		for i in sp.distributions:
 			self.assertEqual(i, [[4, 3], [2, 1], [1, 1]])
 
+	def test_get_mu_sigma(self):
+		# 5 problems, 2 hidden layers
+		sp = StructureProbabilities(5, 2, [5, 0], [4, 1])
+		for problem, probs in sp.distributions:
+			self.assertEqual(sp.get_mu_sigma(problem, 0), [5, 4])
+			self.assertEqual(sp.get_mu_sigma(problem, 1), [0, 1])
+
+		sp.distributions[2][0] = [4, 3]
+		self.assertEqual(sp.get_mu_sigma(2, 0), [4, 3])
+		self.assertEqual(sp.get_mu_sigma(2, 1), [0, 1])
+		self.assertEqual(sp.get_mu_sigma(3, 0), [5, 4])
+
+	def test_set_mu_sigma(self):
+		sp = StructureProbabilities(5, 2, [5, 0], [4, 1])
+		self.assertEqual(sp.get_mu_sigma(2, 0), [5, 4])
+		self.assertEqual(sp.get_mu_sigma(2, 1), [0, 1])
+
+		sp.set_mu_sigma(2, 1, 4, 3)
+		self.assertEqual(sp.get_mu_sigma(2, 0), [4, 3])
+		self.assertEqual(sp.get_mu_sigma(2, 1), [0, 1])
+
+		self.assertEqual(sp.get_mu_sigma(3, 1), [5, 4])
+		sp.set_mu_sigma(2, 2, 1, 1)
+		self.assertEqual(sp.get_mu_sigma(2, 2), [1, 1])
+
 	def test_update_probability(self):
 		# 5 problems, 2 hidden layers
-		sp = StructureProbabilities(5, 2)
+		sp = StructureProbabilities(5, 2, [5, 0], [4, 1])
 		# move away from this structure
-		self.update_probability((5, 1), 1, False)
+		new_mu, new_sigma = sp.update_probability(6, 5, 5, False)
+		assert(new_mu == 4.0)
+		assert(round(new_sigma*1e4)/1.0e4 == 4.0396)
+
+		# move towards this structure
+		new_mu, new_sigma = sp.update_probability(6, 5, 5, True)
+		assert(new_mu == 6.0)
+		assert(round(new_sigma*1e4)/1.0e4 == 3.9223)
+
+	def test_update_probabilities(self):
+		# 5 problems, 2 hidden layers
+		sp = StructureProbabilities(5, 2, [5, 0], [4, 1])
+		# move away from this structure
+		sp.update_probabilities((6, 2), 2, False)
 		for i, probs in enumerate(sp.distributions):
-			if i != 1:
+			if i != 2:
 				self.assertEqual(probs, [[5, 4], [0, 1]])
-		self.assertEqual(sp.distributions[i], [[5 + 4/3.0, 4], [0, 0.8]])
-		
-		
+		self.assertEqual(sp.distributions[2], [[4.0, 4.0396], [0, 1.0050]])
+
+		# move away from this structure again
+		sp.update_probabilities((6, 2), 2, False)
+		for i, probs in enumerate(sp.distributions):
+			if i != 2:
+				self.assertEqual(probs, [[5, 4], [0, 1]])
+		self.assertEqual(sp.distributions[2], [[2.0, 4.0597], [0, 1.0100]])
+
+		# move back towards it
+		sp.update_probabilities((6, 2), 2, True)
+		for i, probs in enumerate(sp.distributions):
+			if i != 2:
+				self.assertEqual(probs, [[5, 4], [0, 1]])
+		self.assertEqual(sp.distributions[2], [[6.0, 4.0396], [2, 1.0]])
+
+	def test_get_sample(self):
+		# 5 problems, 2 hidden layers
+		sp = StructureProbabilities(5, 2, [5, 0], [4, 1])
+		sample = sp.get_sample(2, 0)
+		self.assertTrue(type(sample) == int)
+		self.assertTrue(sample > 0)
+
+		sp = StructureProbabilities(5, 2, [5, 0], [4, 1])
+		sample = sp.get_sample(2, 1)
+		self.assertTrue(type(sample) == int)
+		self.assertTrue(sample >= 0)
 
 if __name__ == '__main__':
 	unittest.main()
